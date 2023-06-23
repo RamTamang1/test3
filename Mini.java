8318086ceb64581f3740204b7696ac082ade9a8e
RamTamang1 <122420578+RamTamang1@users.noreply.github.com>
2023-06-23 15:16:16 -0400

    added Mini.java

---
+++ Mini.java
@@ -0,0 +1,29 @@
+public class Demo1 {
+	
+	
+	void display() {
+		
+		int x=10;
+		int y=0;
+		
+		if (x<y)
+			System.out.println("It is greater than zero");
+		
+			else
+				System.out.println("It is not greater than Zero");
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Demo1 g=new Demo1();
+		g.display();
+		
+		
+		
+
+	}
+
+}
